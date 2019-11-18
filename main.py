24d61c2db1d91e65dd94189224bd03e8e1417da7
Tronhjem <christian@melodrive.com>
2019-11-18 18:08:53 +0100

    tweaks to normnalise and include LU range target

--- main.py
+++ main.py
@@ -3,43 +3,51 @@ import printStats as ps
 import os
 import sys
 
+
 def normaliseFile(path, file):
-    '''takes a path and and file name
+    """takes a path and and file name
     Normalises audio file to -21LUFS with -3dB peak
-    set to 44100hZ'''
-    #TODO add variable target level, true peak and sample rate.
+    set to 44100hZ"""
+    # TODO add variable target level, true peak and sample rate.
 
     print("analysing audio for " + file)
     ffmpeg_normalize = FFmpegNormalize(
-        target_level=-21, true_peak=-3.3, sample_rate=44100
+        target_level=-20.8,
+        true_peak=-3.5,
+        sample_rate=44100,
+        loudness_range_target=5.3,
+        progress=True,
     )
-    ffmpeg_normalize.add_media_file(file, path+'/normalised/'+os.path.basename(file))
+    ffmpeg_normalize.add_media_file(
+        file, path + "/normalised/" + os.path.basename(file)
+    )
     ffmpeg_normalize.run_normalization()
     print("done for " + file)
     print("_")
 
 
 def normaliseAudioFolder(path):
-    '''Takes a path and loops through all .wav files in that path.'''
+    """Takes a path and loops through all .wav files in that path."""
     fileList = os.listdir(path)
     print("================================================")
     print("NORMALISING...")
     print("================================================")
     for file in fileList:
         filePath = os.path.join(path, file)
-        splitFileName = os.path.basename(file).split('.')
+        splitFileName = os.path.basename(file).split(".")
         fileType = splitFileName[len(splitFileName) - 1]
 
-        if fileType == 'wav':
+        if fileType == "wav":
             normaliseFile(path, filePath)
     print("================================================")
     print("DONE NORMALISING")
     print("================================================")
 
+
 if __name__ == "__main__":
-    '''normalise and prints stats for all files in path from sys.argv'''
+    """normalise and prints stats for all files in path from sys.argv"""
     path = sys.argv[1]
-    pathToNorm = path + '/normalised/'
+    pathToNorm = path + "/normalised/"
     if not os.path.exists(pathToNorm):
         os.makedirs(pathToNorm)
 
